0677eb724ceed266cc9bab34ad9847ab9f6ad003
parbhez <masud.affb@gamil.com>
2022-03-20 21:29:23 +0600

    Codeigniter Ecommerce Project

---
+++ application/views/admin/pages/home.php
@@ -0,0 +1,53 @@
+<!-- start: Content -->
+<div id="content" class="span10">
+
+
+    <ul class="breadcrumb">
+        <li>
+            <i class="icon-home"></i>
+            <a href="<?php echo base_url('dashoard')?>">Home</a> 
+            <i class="icon-angle-right"></i>
+        </li>
+        <li><a href="#">Dashboard</a></li>
+    </ul>
+
+    <div class="row-fluid">	
+
+        <a class="quick-button metro yellow span2">
+            <i class="icon-group"></i>
+            <p>Users</p>
+            <span class="badge">237</span>
+        </a>
+        <a class="quick-button metro red span2">
+            <i class="icon-comments-alt"></i>
+            <p>Comments</p>
+            <span class="badge">46</span>
+        </a>
+        <a class="quick-button metro blue span2">
+            <i class="icon-shopping-cart"></i>
+            <p>Orders</p>
+            <span class="badge">13</span>
+        </a>
+        <a class="quick-button metro green span2">
+            <i class="icon-barcode"></i>
+            <p>Products</p>
+        </a>
+        <a class="quick-button metro pink span2">
+            <i class="icon-envelope"></i>
+            <p>Messages</p>
+            <span class="badge">88</span>
+        </a>
+        <a class="quick-button metro black span2">
+            <i class="icon-calendar"></i>
+            <p>Calendar</p>
+        </a>
+
+        <div class="clearfix"></div>
+
+    </div><!--/row-->
+
+
+
+</div><!--/.fluid-container-->
+
+<!-- end: Content -->
